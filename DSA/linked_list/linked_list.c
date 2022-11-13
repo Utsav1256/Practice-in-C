7a00068c703cd8d2609ebbc6f7f9497008db2bc7
Utsav <utsavkumar5462@gmail.com>
2022-11-13 18:04:42 +0530

    make a finction to inser a node at the beginning

--- DSA/linked_list/linked_list.c
+++ DSA/linked_list/linked_list.c
@@ -16,18 +16,36 @@ void printLinkedlist(struct node *p) {
         p = p -> next;
     }
 }
+ 
+void insertAtBeginning(struct node *head) {
+    // Allocating memory space
+    new = (struct node*)malloc(sizeof(struct node));
+  
+    // creating new node
+    printf("\nEnter value: ");
+    scanf("%d", &new->data);
+    new -> next = NULL;
+
+    // inserting the new node add the beginning of the linked list
+    new -> next = head;
+    head = new;
+    // head and new are just pointers, not the node itself.
+
+    printLinkedlist(head);
+} 
 
 int main() {
     int choice; //1->yes/0->no
     
     do
     {
-    // Creating new node 
+    // Creating linked list
         // Allocating memory space
         new = (struct node*)malloc(sizeof(struct node));
         // (struct node*) -> typecasting -> bcz. datatype of *new is struct node
 
-        printf("Enter value: ");
+        // creating new node
+        printf("\nEnter value: ");
         scanf("%d", &new->data);
         new -> next = NULL;
 
@@ -41,11 +59,35 @@ int main() {
             temp = temp -> next;
         }
 
-        printf("want to add one more node? (1->yes/0->no): ");
+        printf("\nwant to add one more node? (1->yes/0->no): ");
         scanf("%d", &choice);
-    } 
-    while (choice == 1);
+    }  while (choice == 1);
     
     // Traversing in list
     printLinkedlist(head);
+
+    // LinkedList Operations
+    printf("\nLinkedList operation: ");
+    do
+    {
+        printf("\n1. Insert at the Beginning \n2. Exit");
+        printf("\nEnter the choice: ");
+        scanf("%d", &choice);
+        switch(choice) {
+            
+            case 1: {
+                insertAtBeginning(head);
+                break;
+            }
+            case 2: {
+                printf("\nExit point");
+                break;
+            }
+            default: {
+                printf("\nPlease Enter a Valid Choice(1/2)");
+            }
+    }
+    }  while (choice != 2);
+    
+   
 }
